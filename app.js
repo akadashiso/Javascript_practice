94698af3e1e88de0cf78d59fab104f2195777b3e
akadashiso <akadashiso@gmail.com>
2021-03-05 01:17:33 +0900

    Finished Javascript Tutorial Ep.2

---
+++ app.js
@@ -0,0 +1,45 @@
+var i;
+var num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です。');
+
+
+// var max = 100;
+// var num = 1;
+// var count = 0;
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+
+// var orrange = 100;
+// var apple = 120;
+
+// if (orrange < apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if(orrange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+
+// var hello ='Hello Worldお';
+// alert(hello);
+
+// alert(400 + 20);
+
+
+// alert('Hello' + 'Worldお');
+
+
+// var str1 = 'Hello';
+// var str2 = 'Worldお!!';
+// alert(str1 + str2);
